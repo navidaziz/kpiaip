cd3e53c42ea7ce96842c152d7e51f3cde44e806c
navidaziz98 <navidaziz98@gmail.com>
2024-11-07 19:24:23 +0500

    some changes

--- application/views/admin/water_user_associations/schemes_list.php
+++ application/views/admin/water_user_associations/schemes_list.php
@@ -25,124 +25,124 @@
     </table>
 
     <script type="text/javascript">
-    $(document).ready(function() {
-        document.title =
-            "<?php echo $schemestatus; ?> Schemes lists (Date:<?php echo date('d-m-Y h:m:s') ?>)";
-        $("#datatable").DataTable({
-            "processing": true,
-            "serverSide": true,
-            "ajax": {
-                "url": "<?php echo base_url(ADMIN_DIR . "water_user_associations/scheme_lists"); ?>",
-                "type": "POST",
-                data: {
-                    scheme_status: '<?php echo $schemestatus; ?>',
+        $(document).ready(function() {
+            document.title =
+                "<?php echo $schemestatus; ?> Schemes lists (Date:<?php echo date('d-m-Y h:m:s') ?>)";
+            $("#datatable").DataTable({
+                "processing": true,
+                "serverSide": true,
+                "ajax": {
+                    "url": "<?php echo base_url(ADMIN_DIR . "water_user_associations/scheme_lists"); ?>",
+                    "type": "POST",
+                    data: {
+                        scheme_status: '<?php echo $schemestatus; ?>',
+                    },
                 },
-            },
-            "columns": [{
-                    "data": null,
-                    "render": function(data, type, row, meta) {
-                        return meta.row + meta.settings
-                            ._iDisplayStart +
-                            1; // Start index from 1
+                "columns": [{
+                        "data": null,
+                        "render": function(data, type, row, meta) {
+                            return meta.row + meta.settings
+                                ._iDisplayStart +
+                                1; // Start index from 1
+                        }
+                    },
+
+                    {
+                        "data": "district_name"
+                    },
+
+                    {
+                        "data": "wua_reg_code"
+                    },
+
+                    {
+                        "data": "wua_name"
+                    },
+                    {
+                        "data": "financial_year"
+                    },
+                    {
+                        "data": "scheme_code"
+                    },
+
+                    {
+                        "data": "scheme_name"
+                    },
+
+                    {
+                        "data": "component_category"
+                    },
+
+                    {
+                        "data": "sanctioned_cost",
+                        "render": function(data, type, row) {
+                            // Format the number here
+                            return parseFloat(data).toLocaleString(
+                                'en-US', {
+                                    minimumFractionDigits: 2
+                                });
+                        }
+                    },
+
+                    {
+                        "data": "paid",
+                        "render": function(data, type, row) {
+                            // Format the number here
+                            return parseFloat(data).toLocaleString(
+                                'en-US', {
+                                    minimumFractionDigits: 2
+                                });
+                        }
+                    },
+
+                    {
+                        "data": "paid_percentage"
+                    },
+
+                    {
+                        "data": "remaining",
+                        "render": function(data, type, row) {
+                            // Format the number here
+                            return parseFloat(data).toLocaleString(
+                                'en-US', {
+                                    minimumFractionDigits: 2
+                                });
+                        }
+                    },
+
+                    {
+                        "data": "payment_count"
+                    },
+                    {
+                        "data": "scheme_note"
+                    },
+
+
+                    {
+                        "data": null,
+                        "render": function(data, type, row) {
+                            return '<a class="llink llink-edit" href="<?php echo site_url(ADMIN_DIR . "water_user_associations/view_scheme_detail/"); ?>' +
+                                row.wua_id + '/' + row.scheme_id +
+                                '"><i class="fa fa-eye"></i> View</a>';
+                        }
                     }
-                },
 
-                {
-                    "data": "district_name"
-                },
+                ],
+                "lengthMenu": [
+                    [15, 25, 50, -1],
+                    [15, 25, 50, "All"]
+                ],
+                "order": [
+                    [6, "asc"]
+                ],
+                "searching": true,
+                "paging": true,
+                "info": true,
+                dom: "Bfrtip",
 
-                {
-                    "data": "wua_reg_code"
-                },
-
-                {
-                    "data": "wua_name"
-                },
-                {
-                    "data": "financial_year"
-                },
-                {
-                    "data": "scheme_code"
-                },
-
-                {
-                    "data": "scheme_name"
-                },
-
-                {
-                    "data": "component_category"
-                },
-
-                {
-                    "data": "sanctioned_cost",
-                    "render": function(data, type, row) {
-                        // Format the number here
-                        return parseFloat(data).toLocaleString(
-                            'en-US', {
-                                minimumFractionDigits: 2
-                            });
-                    }
-                },
-
-                {
-                    "data": "paid",
-                    "render": function(data, type, row) {
-                        // Format the number here
-                        return parseFloat(data).toLocaleString(
-                            'en-US', {
-                                minimumFractionDigits: 2
-                            });
-                    }
-                },
-
-                {
-                    "data": "paid_percentage"
-                },
-
-                {
-                    "data": "remaining",
-                    "render": function(data, type, row) {
-                        // Format the number here
-                        return parseFloat(data).toLocaleString(
-                            'en-US', {
-                                minimumFractionDigits: 2
-                            });
-                    }
-                },
-
-                {
-                    "data": "payment_count"
-                },
-                {
-                    "data": "scheme_note"
-                },
-
-
-                {
-                    "data": null,
-                    "render": function(data, type, row) {
-                        return '<a class="llink llink-edit" href="<?php echo site_url(ADMIN_DIR . "water_user_associations/view_scheme_detail/"); ?>' +
-                            row.wua_id + '/' + row.scheme_id +
-                            '"><i class="fa fa-eye"></i> View</a>';
-                    }
-                }
-
-            ],
-            "lengthMenu": [
-                [15, 25, 50, -1],
-                [15, 25, 50, "All"]
-            ],
-            "order": [
-                [0, "asc"]
-            ],
-            "searching": true,
-            "paging": true,
-            "info": true,
-            dom: "Bfrtip",
-
-            buttons: ["excel", "pageLength"]
+                buttons: ["excel", "pageLength"]
+            });
         });
-    });
     </script>
 
 
@@ -152,32 +152,32 @@
 
 
 <script>
-title = "Expenses";
-$(document).ready(function() {
-    $('#db_table').DataTable({
-        dom: 'Bfrtip',
-        paging: false,
-        title: title,
-        "order": [],
-        searching: true,
-        buttons: [
+    title = "Expenses";
+    $(document).ready(function() {
+        $('#db_table').DataTable({
+            dom: 'Bfrtip',
+            paging: false,
+            title: title,
+            "order": [],
+            searching: true,
+            buttons: [
 
-            {
-                extend: 'print',
-                title: title,
-            },
-            {
-                extend: 'excelHtml5',
-                title: title,
+                {
+                    extend: 'print',
+                    title: title,
+                },
+                {
+                    extend: 'excelHtml5',
+                    title: title,
 
-            },
-            {
-                extend: 'pdfHtml5',
-                title: title,
-                pageSize: 'A4',
+                },
+                {
+                    extend: 'pdfHtml5',
+                    title: title,
+                    pageSize: 'A4',
 
-            }
-        ]
+                }
+            ]
+        });
     });
-});
 </script>
