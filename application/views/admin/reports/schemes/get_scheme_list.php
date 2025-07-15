d288971f7436a15aeb7224936d38a48e1a71085b
Navid Aziz <navidaziz98@gmail.com>
2025-07-15 12:26:34 +0500

    some changes

--- application/views/admin/reports/schemes/get_scheme_list.php
+++ application/views/admin/reports/schemes/get_scheme_list.php
@@ -21,7 +21,7 @@
                 <th>ICR-I&II</th>
                 <th>Other</th>
                 <th>FCR</th>
-                <th>Physical Complete</th>
+                <th>Physical Completed</th>
                 <th>Action</th>
             </tr>
         </thead>
