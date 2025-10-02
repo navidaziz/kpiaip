0ead9f49469d6a9d94e37ea49e623fef85206b89
Navid Aziz <navidaziz98@gmail.com>
2025-10-02 17:25:59 +0500

    some changes

--- application/views/admin/water_user_associations/view_scheme_detail.php
+++ application/views/admin/water_user_associations/view_scheme_detail.php
@@ -953,7 +953,7 @@
 
                             <?php if ($this->session->userdata('role_id') == 4) { ?>
                                 <button onclick="initiate_scheme(<?php echo $scheme->scheme_id ?>)"
-                                    class="btn btn-danger btn-sm"><i class="fa fa-forward"></i>
+                                    class="btn btn-danger btn-sm"><i class="fa fa-edit"></i>
                                     Edit Scheme Detail
                                 </button>
                             <?php } ?>
